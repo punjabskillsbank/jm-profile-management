806166ad5accc9827cf574f69cb68f5d3adc8823
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2025-02-17 00:29:10 -0500

    initial commit for the repo

---
+++ src/main/java/com/jobmatrix/jm_profile_management/JmProfileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_profile_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmProfileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmProfileManagementApplication.class, args);
+	}
+
+}
