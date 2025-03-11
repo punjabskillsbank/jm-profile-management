27199e75db3c808d0241775ccce18cb2f94f9044
Ansh Preet Kaur <ansh0043.becse24@chitkara.edu.in>
2025-03-11 17:44:09 +0530

    JMK_26: setting up swagger in jm_profile_management

--- src/main/java/com/jobmatrix/controller/FreelancerProfileController.java
+++ src/main/java/com/jobmatrix/controller/FreelancerProfileController.java
@@ -2,6 +2,8 @@ package com.jobmatrix.controller;
 
 import com.jobmatrix.dto.FreelancerDTO;
 import com.jobmatrix.service.FreelancerProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/freelancer")
 @RequiredArgsConstructor
+@Tag(name = "Freelancer Profile Management", description = "Operations related to freelancer profile management")
 public class FreelancerProfileController {
 
     private final FreelancerProfileService freelancerProfileService;
 
     // POST mapping to save the freelancer profile data in database
+    @Operation(summary = "Create a new freelancer profile", description = "Save the freelancer profile data in the database")
     @PostMapping("/create_profile")
     public ResponseEntity<FreelancerDTO> createProfile(@Valid @RequestBody FreelancerDTO freelancerDTO) {
         FreelancerDTO dto = freelancerProfileService.createFreelancerProfile(freelancerDTO);
