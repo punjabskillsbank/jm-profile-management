92559373e06ef1146cc5d2a30444e226ba6a5880
Ansh Preet Kaur <ansh0043.becse24@chitkara.edu.in>
2025-03-12 11:26:38 +0530

    JMK_26: update file name swaggerConfig to SwaggerConfig

--- src/main/java/com/jobmatrix/config/SwaggerConfig.java
+++ src/main/java/com/jobmatrix/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class swaggerConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
