7f71f3c67aacb15fb54f42a8d217eff7b674a529
Ansh Preet Kaur <ansh0043.becse24@chitkara.edu.in>
2025-04-25 17:14:11 +0530

    JMK_56: enable CORS support, update swagger version

--- src/main/java/com/jobmatrix/JmProfileManagementApplication.java
+++ src/main/java/com/jobmatrix/JmProfileManagementApplication.java
@@ -1,10 +1,13 @@
 package com.jobmatrix;
 
+import com.common.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 @EntityScan(basePackages = {"com.jobmatrix", "com.common.entity"})
 public class JmProfileManagementApplication {
 
