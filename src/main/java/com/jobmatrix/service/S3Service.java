77514e4e4841828f207e5d62d7f79eb55f2b408c
pawanpreet193 <pawanpreetsingh193@gmail.com>
2025-05-29 16:07:24 +0530

    JMK_85: removed unnecessary comments

--- src/main/java/com/jobmatrix/service/S3Service.java
+++ src/main/java/com/jobmatrix/service/S3Service.java
@@ -6,17 +6,6 @@ import java.net.URL;
 public interface S3Service {
 
     /**
-     * Uploads a file to S3 bucket and returns the pre-signed URL to access the file
-     * This URL will expire after a set period
-     *
-     * @param fileName the name to give the file in S3
-     * @param inputStream the file content as an input stream
-     * @param contentType the MIME type of the file
-     * @return the pre-signed URL to access the uploaded file
-     */
-    //String uploadFile(String fileName, InputStream inputStream, String contentType);
-
-    /**
      * Generates a presigned URL for uploading a file to S3
      *
      * @param fileName the name to give the file in S3
@@ -24,6 +13,5 @@ public interface S3Service {
      * @return the presigned URL for uploading
      */
     URL generatePresignedUploadUrl(String fileName, String contentType);
-
 
 }
