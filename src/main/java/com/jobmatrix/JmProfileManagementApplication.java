32f3097c89e73095332dae3ae50ab75bca067af4
Ansh27122006 <kauransh2006@gmail.com>
2025-08-31 08:59:16 +0530

    JMK_119: shift S3Config.java to common

--- src/main/java/com/jobmatrix/JmProfileManagementApplication.java
+++ src/main/java/com/jobmatrix/JmProfileManagementApplication.java
@@ -2,13 +2,14 @@ package com.jobmatrix;
 
 import com.common.config.AwsConfig;
 import com.common.config.CorsConfig;
+import com.common.config.S3Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({CorsConfig.class, AwsConfig.class})
+@Import({CorsConfig.class, AwsConfig.class, S3Config.class})
 @EntityScan(basePackages = {"com.common.entity"})
 public class JmProfileManagementApplication {
 
